7a1f5201f374eccff3506f0871b4bd8805bdbee0
mukta81 <168319607+mukta81@users.noreply.github.com>
2024-04-28 05:07:26 +0200

    Add files via upload
    Day 1

---
+++ day1/Firefox.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+import java.nio.file.spi.FileSystemProvider;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+
+
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true; 
+		int releaseYear = 1998; 
+		char browserLogo = 'f';
+		
+		System.out.println("Browser:" + browserVersion);
+		System.out.println("Browser Name:" +browserName);
+		System.out.println("Visible:" +isVisible);
+		System.out.println("Release Year:" +releaseYear);
+		System.out.println("Browser Logo:" +browserLogo);
+
+	
+	}
+
+}
