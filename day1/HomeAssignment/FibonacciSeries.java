dcc100b847eb99de09a26f179741451b40e01e25
mukta81 <168319607+mukta81@users.noreply.github.com>
2024-04-28 16:35:52 +0200

    Add files via upload
    Added Repository for Home Assignements from TestLeaf Website Assignments

---
+++ day1/HomeAssignment/FibonacciSeries.java
@@ -0,0 +1,24 @@
+package week1.day1.HomeAssignment;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		
+		int firstNumber=0;
+		int secondNumber=1;
+		int range=8;
+		
+		System.out.print(firstNumber+","+secondNumber);
+		
+		
+		for(int i=1; i<range-1;i++)
+		{
+			int nextNumber=firstNumber+secondNumber;
+			System.out.print(","+ nextNumber);
+			firstNumber=secondNumber;
+			secondNumber=nextNumber;
+		}
+
+	}
+
+}
